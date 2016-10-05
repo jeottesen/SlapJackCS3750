64aa2b3b5933f0b165eb1c7eb96ec12722758d2f
Illidan <Illidan@DESKTOP-9KD2VMI>
2016-10-05 10:21:42 -0600

    Created board and player cs files

---
+++ SlapJackCS3750/SlapJackCS3750/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlapJackCS3750
+{
+    class Player
+    {
+    }
+}
